b3d866451d2deb0c91f571982c058012d83c6dc5
Your Name <you@example.com>
2024-01-15 00:54:13 +0100

    Upgraded Healthbar UI

--- Assets/Scripts/UI/HealthBarUIManager.cs
+++ Assets/Scripts/UI/HealthBarUIManager.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class HealthBarUIManager : MonoBehaviour
 {
     GameObject player;
-    [SerializeField] public Image healthBar;
-    [SerializeField] private float fillSpeed;
+    [SerializeField] private float lerpSpeed;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Gradient colorGradient;
+    [SerializeField] private Text healthCounter;
 
     // Update is called once per frame
     void Update()
     {
         player = GameObject.FindWithTag("Player");
-        
+        lerpSpeed = 3f * Time.deltaTime;
+        float TargetHealth = player.GetComponent<HealthSystem>().health / player.GetComponent<HealthSystem>().maxHealth;
         if (player != null) {
-                healthBar.fillAmount = Mathf.Clamp(player.GetComponent<HealthSystem>().health / player.GetComponent<HealthSystem>().maxHealth, fillSpeed, 1);
+                healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, TargetHealth, lerpSpeed);
+                healthCounter.text = TargetHealth * 100 + "%";
+                healthBar.color = colorGradient.Evaluate(TargetHealth);
         }
     }
 }
